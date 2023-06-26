a8309756ee8b4c5d3c80d0c4aa0d141ba8e74d9b
AgrawalKashish <96725005+AgrawalKashish@users.noreply.github.com>
2023-06-26 17:01:52 +0530

    integerated

--- mernapp/backend/db.js
+++ mernapp/backend/db.js
@@ -7,8 +7,11 @@ const mongodb = async () => {
     console.log("connected to mongodb");
     const fetched_data = await mongoose.connection.db.collection("food_items").find({}).toArray();
       // console.log(fetched_data);
-      global.food_items = fetched_data;
+    global.food_items = fetched_data;
       // console.log(global.food_items)
+    const fetched_data1 = await mongoose.connection.db.collection("food_category").find({}).toArray();
+    global.food_category = fetched_data1;
+    //console.log(global.food_category)
   }
    catch (err) {
     console.log("error", err);
--- mernapp/src/components/cart.js
+++ mernapp/src/components/cart.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-export default function Cart() {
+export default function Cart(props) {
+  let options = props.options;
+  let priceOptions = Object.keys(options);
   return (
     <div>
         <div
           className="card mt-3"
           style={{ width: "18rem", maxHeight: "360px" }}
         >
-          <img src="https://source.unsplash.com/random/3000x3000/?swiggy" style={{width: "100%", height: "100%", overflow: "hidden"}} className="card-img-top" alt="..." />
+          <img src={props.imgSrc} style={{width: "100%", height: "100%", overflow: "hidden"}} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">card's content.</p>
+             <h5 className="card-title">{props.foodName}</h5> 
             <div className="container w-100">
               <select className="m-2 h-100  bg-success-rounded">
                 {Array.from(Array(6), (e, i) => {
@@ -22,8 +23,15 @@ export default function Cart() {
                 })}
               </select>
               <select className="m-2 h-100 bg-success-rounded">
-                <option value="half">Half</option>
-                <option value="full">Full</option>
+               {priceOptions.map((data) => {
+                  return (
+                    <option key={data} value={data}>
+                      {data}
+                    </option>
+                  );
+                }
+                )
+               }
               </select>
               <div className="d-inline h-100 fs-4"></div>
             </div>
